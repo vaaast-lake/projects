998cc2f1c6a186e260bb9cd309cedc534285c8ca
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2023-01-12 20:37:37 +0900

    filtering_e

--- src/AppTodo_e.jsx
+++ src/AppTodo_e.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/ellie/Header/Header';
 import TodoList from './components/ellie/TodoList/TodoList';
 
+const filters = ['all', 'active', 'completed']
+
 export default function AppTodo_e() {
+  const [filter, setFilter] = useState(filters[0]);
 
   return (
     <div>
-      <TodoList />
+      <Header 
+        filters={filters} 
+        filter={filter} 
+        onFilterChange={setFilter} 
+      />
+      <TodoList filter={filter} />
     </div>
   );
 }
--- src/components/ellie/TodoList/TodoList.jsx
+++ src/components/ellie/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: '123', text: '장보기', status: 'active' },
     { id: '124', text: '공부하기', status: 'active' },
@@ -18,11 +18,13 @@ export default function TodoList() {
   const handleDelete = (deleted) => 
     setTodos(todos.filter(t => t.id !== deleted.id))
 
+  const filtered = getFilteredItems(todos, filter);
+
   return (
     <section>
       <ul>
         {
-          todos.map(item => (
+          filtered.map(item => (
             <Todo
               todo={item}
               onUpdate={handleUpdate}
@@ -36,3 +38,9 @@ export default function TodoList() {
   );
 }
 
+function getFilteredItems(todos, filter) {
+  if(filter === 'all') {
+    return todos;
+  }
+  return todos.filter(todo => todo.status === filter);
+}
